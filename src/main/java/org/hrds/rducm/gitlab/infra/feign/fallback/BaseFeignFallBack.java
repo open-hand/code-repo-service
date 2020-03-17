ef6c13836ab81063238afc2f44b3f527382053b1
谢颖 <ying.xie@hand-china.com>
2020-03-17 18:13:56 +0800

    [FIX] 一些接口改为feign调用

--- src/main/java/org/hrds/rducm/gitlab/infra/feign/fallback/BaseFeignFallBack.java
+++ src/main/java/org/hrds/rducm/gitlab/infra/feign/fallback/BaseFeignFallBack.java
@@ -1,16 +1,11 @@
 package org.hrds.rducm.gitlab.infra.feign.fallback;
 
-import org.hrds.rducm.gitlab.infra.feign.BaseFeign;
-import org.hrds.rducm.gitlab.infra.feign.vo.UserDTO;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-
 /**
  * @author ying.xie@hand-china.com
  * @date 2020/3/16
  */
 //@Component
-//public class BaseFeignFallBack implements BaseFeign {
+//public class BaseFeignFallBack implements BaseServiceFeignClient {
 //    @Override
 //    public ResponseEntity<UserDTO> query(Long organizationId, Long id) {
 //        throw new RuntimeException();
