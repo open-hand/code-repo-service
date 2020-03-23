6c62b626f2b0d91fa29d38c77e786025ab122f2d
谢颖 <ying.xie@hand-china.com>
2020-03-23 20:43:55 +0800

    [ADD] 限制Gitlab 客户端的权限

--- src/main/java/org/hrds/rducm/gitlab/infra/client/gitlab/Gitlab4jClientWrapper.java
+++ src/main/java/org/hrds/rducm/gitlab/infra/client/gitlab/Gitlab4jClientWrapper.java
@@ -6,7 +6,10 @@ import org.gitlab4j.api.GitLabApi;
 import org.hrds.rducm.gitlab.domain.entity.RdmUser;
 import org.hrds.rducm.gitlab.domain.repository.RdmUserRepository;
 import org.hrds.rducm.gitlab.domain.service.IC7nBaseServiceService;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -15,18 +18,25 @@ import java.util.Objects;
  * 临时使用, 后续需删除 fixme
  */
 @Component
-public class Gitlab4jClientWrapper extends Gitlab4jClient {
+public class Gitlab4jClientWrapper extends Gitlab4jClient implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
+
     @Override
     public GitLabApi getGitLabApi() {
         // 转换Gitlab userId todo
         Long userId = DetailsHelper.getUserDetails().getUserId();
 
         // 获取Gitlab用户id
-        RdmUserRepository rdmUserRepository = ApplicationContextHelper.getApplicationContext().getBean(RdmUserRepository.class);
+        RdmUserRepository rdmUserRepository = applicationContext.getBean(RdmUserRepository.class);
         RdmUser dbUser = rdmUserRepository.selectByUk(userId);
         Integer glUserId = Objects.requireNonNull(dbUser.getGlUserId());
 
         // 获取当前用户的gitlab客户端
         return getGitLabApiUser(glUserId);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
