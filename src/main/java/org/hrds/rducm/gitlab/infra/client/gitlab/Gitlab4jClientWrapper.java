3c05f5abc899ce76db557b4f8cb6b9e7665e5347
谢颖 <ying.xie@hand-china.com>
2020-03-23 16:55:00 +0800

    [ADD] 限制Gitlab 客户端的权限

--- src/main/java/org/hrds/rducm/gitlab/infra/client/gitlab/Gitlab4jClientWrapper.java
+++ src/main/java/org/hrds/rducm/gitlab/infra/client/gitlab/Gitlab4jClientWrapper.java
@@ -1,16 +1,33 @@
 package org.hrds.rducm.gitlab.infra.client.gitlab;
 
+import io.choerodon.core.oauth.DetailsHelper;
 import org.gitlab4j.api.GitLabApi;
+import org.hrds.rducm.gitlab.domain.entity.RdmUser;
+import org.hrds.rducm.gitlab.domain.repository.RdmUserRepository;
+import org.hrds.rducm.gitlab.domain.service.IC7nBaseServiceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 /**
  * 临时使用, 后续需删除 fixme
  */
 @Component
 public class Gitlab4jClientWrapper extends Gitlab4jClient {
+    @Autowired
+    private RdmUserRepository rdmUserRepository;
+
     @Override
     public GitLabApi getGitLabApi() {
+        // 转换Gitlab userId todo
+        Long userId = DetailsHelper.getUserDetails().getUserId();
+
+        // 获取Gitlab用户id
+        RdmUser dbUser = rdmUserRepository.selectByUk(userId);
+        Integer glUserId = Objects.requireNonNull(dbUser.getGlUserId());
+
         // 获取当前用户的gitlab客户端
-        return getGitLabApiUser(null);
+        return getGitLabApiUser(glUserId);
     }
 }
