41c6724858dbed08fae6864df0aa5fcdef5aba60
ying.xie <ying.xie@hand-china.con>
2020-05-12 19:48:32 +0800

    [IMP] 优化

--- src/main/java/org/hrds/rducm/gitlab/api/controller/v1/MemberAuditLogController.java
+++ src/main/java/org/hrds/rducm/gitlab/api/controller/v1/MemberAuditLogController.java
@@ -11,8 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 成员权限审计日志表 管理 API
