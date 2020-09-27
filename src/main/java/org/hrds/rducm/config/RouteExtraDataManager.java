b580a7e7a57ee02740af37471613f5bafeb73fba
limofei <1458908533@qq.com>
2020-09-27 16:40:35 +0800

    [ADD]开源-服务名&包名&配置文件修改

--- src/main/java/org/hrds/rducm/config/RouteExtraDataManager.java
+++ src/main/java/org/hrds/rducm/config/RouteExtraDataManager.java
@@ -19,9 +19,9 @@ public class RouteExtraDataManager implements ExtraDataManager {
     @Override
     public ExtraData getData() {
         ChoerodonRouteData choerodonRouteData = new ChoerodonRouteData();
-        choerodonRouteData.setName(environment.getProperty("hzero.service.current.name", "hrds-code-repo"));
+        choerodonRouteData.setName(environment.getProperty("hzero.service.current.name", "code-repo-service"));
         choerodonRouteData.setPath(environment.getProperty("hzero.service.current.path", "/rducm/**"));
-        choerodonRouteData.setServiceId(environment.getProperty("hzero.service.current.service-name", "hrds-code-repo"));
+        choerodonRouteData.setServiceId(environment.getProperty("hzero.service.current.service-name", "code-repo-service"));
         choerodonRouteData.setPackages("org.hrds");
         extraData.put(ExtraData.ZUUL_ROUTE_DATA, choerodonRouteData);
         return extraData;
