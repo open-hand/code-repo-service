2aeb7be14c6f9ebdb9660d739b5580aa74726051
limofei <1458908533@qq.com>
2020-10-27 20:12:51 +0800

    [FIX]去除无用代码

--- src/main/java/org/hrds/rducm/gitlab/domain/service/IMemberAuditService.java
+++ src/main/java/org/hrds/rducm/gitlab/domain/service/IMemberAuditService.java
@@ -28,14 +28,4 @@ public interface IMemberAuditService {
      */
     void auditMembersByOrganizationId(Long organizationId);
 
-    /**
-     * 根据组织ID修复权限有问题的代码库成员
-     * 1. 以代码库成员权限为准，修复问题数据
-     * 2. 删除修复成功的审计数据
-     * 3. 保留无法修复的审计数据
-     *
-     * @param organizationId
-     * @param memberAuditRecordList
-     */
-    void repairMemberPermission(Long organizationId, List<RdmMemberAuditRecord> memberAuditRecordList);
 }
