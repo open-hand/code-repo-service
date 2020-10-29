c76811bd14a46b0a38c5067b524dc6b8e384b08e
limofei <1458908533@qq.com>
2020-10-29 11:12:04 +0800

    [FIX]审计启用的应用服务

--- src/main/java/org/hrds/rducm/gitlab/app/service/impl/RdmMemberAuditAppServiceImpl.java
+++ src/main/java/org/hrds/rducm/gitlab/app/service/impl/RdmMemberAuditAppServiceImpl.java
@@ -16,6 +16,8 @@ import org.hrds.rducm.gitlab.infra.client.gitlab.model.AccessLevel;
 import org.hrds.rducm.gitlab.infra.feign.vo.C7nDevopsProjectVO;
 import org.hrds.rducm.gitlab.infra.feign.vo.C7nUserVO;
 import org.hrds.rducm.gitlab.infra.util.AssertExtensionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,6 +33,8 @@ import java.util.Objects;
  */
 @Service
 public class RdmMemberAuditAppServiceImpl implements RdmMemberAuditAppService {
+    private static final Logger logger = LoggerFactory.getLogger(RdmMemberAuditAppServiceImpl.class);
+
     @Autowired
     private RdmMemberAuditRecordRepository rdmMemberAuditRecordRepository;
     @Autowired
@@ -105,19 +109,24 @@ public class RdmMemberAuditAppServiceImpl implements RdmMemberAuditAppService {
         RdmMember dbMember = rdmMemberRepository.selectOneByUk(projectId, repositoryId, userId);
         // 查询gitlab权限
         Member projectGlMember = gitlabProjectApi.getMember(glProjectId, glUserId);
+        logger.info("Gl项目[{}]权限，ID为[{}],用户名[{}]的权限级别[{}]", glProjectId, projectGlMember.getId(), projectGlMember.getName(), projectGlMember.getAccessLevel());
         Member groupGlMember = gitlabGroupApi.getMember(glGroupId, glUserId);
+        logger.info("Gl组[{}]权限，ID为[{}],用户名[{}]的权限级别[{}]",glGroupId, projectGlMember.getId(), projectGlMember.getName(), projectGlMember.getAccessLevel());
 
         // 判断是否是组织管理员
         Boolean isOrgAdmin = c7NBaseServiceFacade.checkIsOrgAdmin(organizationId, userId);
+        logger.info("用户[{}]是否为组织管理员[{}]", userId, isOrgAdmin);
 
         // 判断是否是项目团队成员
         C7nUserVO c7nUserVO = c7NBaseServiceFacade.detailC7nUserOnProjectLevel(projectId, userId);
         boolean isProjectMember = c7nUserVO != null;
+        logger.info("用户[{}]是否为项目成员[{}]", userId, isOrgAdmin);
 
         // <> 按gitlab group和project两类情况讨论
         // 是否为组织管理员
         if (isOrgAdmin) {
             // 修复为group Owner权限
+            logger.info("修复用户[{}]为组织管理员权限", userId);
             updateGitlabGroupMemberWithOwner(groupGlMember, glGroupId, glUserId);
         } else {
             // 是否为团队成员
@@ -127,21 +136,28 @@ public class RdmMemberAuditAppServiceImpl implements RdmMemberAuditAppService {
                 Boolean isProjectAdmin = c7nUserVO.isProjectAdmin();
                 if (isProjectAdmin) {
                     // 修复为group Owner权限
+                    logger.info("修复用户[{}]为项目管理员权限", userId);
                     updateGitlabGroupMemberWithOwner(groupGlMember, glGroupId, glUserId);
                 } else {
                     // 修复为该用户当前的代码库权限
                     if (dbMember == null || !dbMember.getSyncGitlabFlag()) {
                         // 移除gitlab权限
+                        logger.info("用户[{}]为项目成员，但没有代码库权限，移除Gl权限", userId);
                         removeGitlabMemberGP(groupGlMember, projectGlMember, glGroupId, glProjectId, glUserId);
                     } else {
                         // 修改gitlab权限
+                        logger.info("用户[{}]为项目成员，有代码库权限，修复Gl权限", userId);
                         updateGitlabMemberP(dbMember, groupGlMember, projectGlMember, glGroupId, glProjectId, glUserId);
                     }
                 }
             } else {
                 if (Objects.isNull(dbMember)) {
                     // 如果不是团队成员,也不是赋予权限的项目外成员 移除gitlab权限
+                    logger.info("用户[{}]是项目成员也不是外部成员，没有代码库权限，移除", userId);
                     removeGitlabMemberGP(groupGlMember, projectGlMember, glGroupId, glProjectId, glUserId);
+                } else {
+                    logger.info("用户[{}]是外部成员，有代码库权限，修复GL权限", userId);
+                    updateGitlabMemberP(dbMember, groupGlMember, projectGlMember, glGroupId, glProjectId, glUserId);
                 }
             }
         }
