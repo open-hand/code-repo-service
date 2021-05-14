64ce7eda59f073f51a85460ef3c2376c6c171f6b
20399 <yanfang.lin@hand-china.com>
2021-05-14 16:53:31 +0800

    [IMP] 修改配置

--- react/config.js
+++ react/config.js
@@ -1,32 +1,18 @@
-const alias = require('../alias').webpack;
-
 const config = {
+  port: 8086,
+  fileServer: 'http://minio.staging.saas.test.com',
+  projectType: 'choerodon',
+  buildType: 'single',
   master: './node_modules/@choerodon/master/lib/master.js',
-  modules: ['.'],
-  resourcesLevel: ['site', 'organization', 'project', 'user'],
-  webpackConfig(configs) {
-    configs.resolve.alias = alias;
-    return configs;
+  theme: {
+    'primary-color': '#3f51b5',
+    'icon-font-size-base': '16px',
   },
+  dashboard: {},
+  modules: [
+    '.',
+  ],
+  resourcesLevel: ['site', 'organization', 'project', 'user'],
 };
 
 module.exports = config;
-
-// const config = {
-//   server: 'http://api.staging.saas.test.com',
-//   fileServer: 'http://minio.staging.saas.test.com',
-//   projectType: 'choerodon',
-//   buildType: 'single',
-//   master: './node_modules/@choerodon/master/lib/master.js',
-//   theme: {
-//     'primary-color': '#3f51b5',
-//     'icon-font-size-base': '16px',
-//   },
-//   dashboard: {},
-//   modules: [
-//     '.',
-//   ],
-//   resourcesLevel: ['site', 'organization', 'project', 'user'],
-// };
-
-// module.exports = config;
