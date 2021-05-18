3cb994317108e7a4069a077a9a99e2982b0d1890
wangxiang <xiangwang04@hand-china.com>
2021-05-18 17:52:24 +0800

    [FIX] 增加批量处理接口

---
+++ src/main/java/org/hrds/rducm/gitlab/api/controller/dto/RdmMemberApplicantPassVO.java
@@ -0,0 +1,31 @@
+package org.hrds.rducm.gitlab.api.controller.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import org.hzero.starter.keyencrypt.core.Encrypt;
+
+/**
+ * Created by wangxiang on 2021/5/18
+ */
+public class RdmMemberApplicantPassVO {
+    @ApiModelProperty("申请记录")
+    @Encrypt
+    private Long id;
+    @ApiModelProperty("obj")
+    private Long objectVersionNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getObjectVersionNumber() {
+        return objectVersionNumber;
+    }
+
+    public void setObjectVersionNumber(Long objectVersionNumber) {
+        this.objectVersionNumber = objectVersionNumber;
+    }
+}
