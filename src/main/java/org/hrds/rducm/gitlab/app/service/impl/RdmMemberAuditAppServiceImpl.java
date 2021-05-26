b4d3c87a192e7a09dca1c3db035aa9d3eefdf954
wangxiang <xiangwang04@hand-china.com>
2021-05-26 17:37:35 +0800

    [FIX] 增加日志

--- src/main/java/org/hrds/rducm/gitlab/app/service/impl/RdmMemberAuditAppServiceImpl.java
+++ src/main/java/org/hrds/rducm/gitlab/app/service/impl/RdmMemberAuditAppServiceImpl.java
@@ -95,6 +95,7 @@ public class RdmMemberAuditAppServiceImpl implements RdmMemberAuditAppService {
 
     @Override
     public void auditFix(Long organizationId, Long projectId, Long repositoryId, Long id) {
+        logger.info(">>>>{}>>>{}>>>>{}>>>>{}>", organizationId, projectId, repositoryId, id);
         RdmMemberAuditRecord dbRecord = rdmMemberAuditRecordRepository.selectByUk(organizationId, projectId, repositoryId, id);
         AssertExtensionUtils.notNull(dbRecord, "该记录不存在");
 
