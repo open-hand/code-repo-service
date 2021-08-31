224ce35e2e5c4904a6c774776e90d9d4a2d07829
jamin <245139263@qq.com>
2021-08-31 18:47:27 +0800

    【fix】- group层级权限 选择的bug

--- react/routes/code-lib/code-management/main-view/modals/ps-set/ModifySider.js
+++ react/routes/code-lib/code-management/main-view/modals/ps-set/ModifySider.js
@@ -12,21 +12,32 @@ export default observer((props) => {
     if (await psSetDs.submit()) {
       psSetDs.query();
       return true;
-    } else {
-      return false;
     }
+    return false;
   }
 
   function getClusterOptionProp({ record }) {
+    const numValue = Number(record.data.value.substring(1));
+    if (numValue >= 50) {
+      return {
+        disabled: true,
+      };
+    }
+    //  打开的是应用服务,并且这个人有全局权限
+    if (psSetDs.current.get('type') === 'project' && psSetDs.current.get('groupAccessLevel')) {
+      return {
+        disabled: psSetDs.current.get('groupAccessLevel') >= numValue,
+      };
+    }
     return {
-      disabled: Number(record.data.value.substring(1)) >= 50,
+      disabled: false,
     };
   }
 
   function renderRole({ value }) {
     return value.join();
   }
-  
+
   modal.handleOk(() => handleOk());
   modal.handleCancel(() => {
     psSetDs.reset();
