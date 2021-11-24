a15cfec4f44ca9d298cde0deadd1f86b63fa74d6
jamin <245139263@qq.com>
2021-11-24 16:05:01 +0800

    [FIX]-修改认证绑定

---
+++ react/routes/personal-setting/third-part-service-bind/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount } from '@choerodon/inject';
+
+const ThirdPartServiceBind = () => {
+    return (
+        <>
+        {mount('base-pro:thirdPartServiceBind', {})}
+        </>
+    )
+}
+
+export default ThirdPartServiceBind
