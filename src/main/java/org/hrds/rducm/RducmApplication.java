22622fc6de37b1213647ce8e9272f150f5e7dec4
汪翔 <xiang.wang04@hand-china.com>
2021-12-24 17:57:21 +0800

    Update RducmApplication.java

--- src/main/java/org/hrds/rducm/RducmApplication.java
+++ src/main/java/org/hrds/rducm/RducmApplication.java
@@ -15,7 +15,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class RducmApplication {
 
     public static void main(String[] args) {
-        
         SpringApplication.run(RducmApplication.class, args);
     }
 }
