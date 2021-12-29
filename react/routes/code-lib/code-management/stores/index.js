8d8308fc361f366c91cfa212a3a8d06bdfe9ea1a
翁恺敏 <736653759@qq.com>
2021-12-29 10:51:47 +0800

    [FIX] fix data

--- react/routes/code-lib/code-management/stores/index.js
+++ react/routes/code-lib/code-management/stores/index.js
@@ -1,7 +1,6 @@
 import React, { createContext, useEffect, useState, useContext, useMemo, useCallback } from 'react';
 import { inject } from 'mobx-react';
 import { injectIntl } from 'react-intl';
-import { useCheckPermission } from '@/utils/index';
 import { checkPermission } from '@/utils';
 import useStore from './useStore';
 
@@ -14,7 +13,7 @@ export function useManagementStore() {
 export const StoreProvider = injectIntl(inject('AppState')((props) => {
   const {
     children,
-    AppState: { currentMenuType: { projectId, type }, getUserId },
+    AppState: { currentMenuType: { projectId, type }, getUserInfo },
   } = props;
 
   const [hasMemberPermission, sethasMemberPermission] = useState(false);
@@ -33,7 +32,7 @@ export const StoreProvider = injectIntl(inject('AppState')((props) => {
   const managementStore = useStore(tabs);
 
   const init = useCallback(async () => {
-    const codePermissions = localStorage.getItem(`codePermissions-${getUserId}-${projectId}`);
+    const codePermissions = localStorage.getItem(`codePermissions-${JSON.stringify(getUserInfo)}-${projectId}`);
     if (codePermissions) {
       try {
         const {
@@ -50,7 +49,7 @@ export const StoreProvider = injectIntl(inject('AppState')((props) => {
       const hasPermission1 = await checkPermission({ projectId, code: ['choerodon.code.project.infra.code-lib-management.ps.project-owner'], resourceType: type });
       sethasMemberPermission(hasMemberPermission1);
       sethasPermission(hasPermission1);
-      localStorage.setItem(`codePermissions-${getUserId}-${projectId}`, JSON.stringify({
+      localStorage.setItem(`codePermissions-${JSON.stringify(getUserInfo)}-${projectId}`, JSON.stringify({
         localMemberPermission: hasMemberPermission1,
         localOwnerPermission: hasPermission1,
       }));
