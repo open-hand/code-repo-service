7f36fabd7a1e30094d48b74d92162d543151bbc4
wangxiang <xiangwang04@hand-china.com>
2022-01-04 16:30:08 +0800

    [FIX] 增加数据修复逻辑

--- src/main/java/org/hrds/rducm/gitlab/app/job/FixMemberTask.java
+++ src/main/java/org/hrds/rducm/gitlab/app/job/FixMemberTask.java
@@ -1,8 +1,7 @@
 package org.hrds.rducm.gitlab.app.job;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 import org.hrds.rducm.gitlab.domain.entity.RdmMember;
 import org.hrds.rducm.gitlab.domain.facade.C7nBaseServiceFacade;
 import org.hrds.rducm.gitlab.domain.facade.C7nDevOpsServiceFacade;
@@ -103,5 +102,62 @@ public class FixMemberTask {
         LOGGER.info(">>>>>>>>>>>>>>>end fix member permission>>>>>>>>>>>>>>>>>>");
     }
 
+
+    @JobTask(maxRetryCount = 3, code = "fixMemberPermissionData", description = "用户权限数据修复")
+    @TimedTask(name = "fixMemberPermissionData", description = "用户权限数据修复", oneExecution = true,
+            repeatCount = 0, repeatInterval = 1, repeatIntervalUnit = QuartzDefinition.SimpleRepeatIntervalUnit.HOURS, params = {})
+    public void fixMemberPermissionData(Map<String, Object> map) {
+        //1.原来所有的owner的权限统统改为group
+        List<C7nTenantVO> c7nTenantVOS = c7nBaseServiceFacade.listAllOrgs();
+        if (CollectionUtils.isEmpty(c7nTenantVOS)) {
+            LOGGER.info("tenant is null");
+        }
+
+        LOGGER.info(">>>>>>>>>>>>>>>start fix member permission data>>>>>>>>>>>>>>>>>>");
+        c7nTenantVOS.forEach(c7nTenantVO -> {
+            Set<Long> projectIds = c7nBaseServiceFacade.listProjectIds(c7nTenantVO.getTenantId());
+            if (!CollectionUtils.isEmpty(projectIds)) {
+                projectIds.forEach(projectId -> {
+                    //查询项目下所有有gitlab owner标签的用户
+                    List<C7nUserVO> c7nUserVOS = c7nBaseServiceFacade.listCustomGitlabOwnerLableUser(projectId, "GITLAB_OWNER");
+                    if (!CollectionUtils.isEmpty(c7nUserVOS)) {
+                        //查询项目组的
+                        Long appGroupIdByProjectId = c7nDevOpsServiceFacade.getAppGroupIdByProjectId(projectId);
+                        c7nUserVOS.forEach(c7nUserVO -> {
+                            RdmMember record = new RdmMember();
+                            record.setType(AuthorityTypeEnum.GROUP.getValue());
+                            record.setProjectId(projectId);
+                            record.setUserId(c7nUserVO.getId());
+                            List<RdmMember> rdmMembers = rdmMemberMapper.select(record);
+                            boolean contains = rdmMembers.stream().map(RdmMember::getGlAccessLevel).collect(Collectors.toList()).contains(50);
+                            List<Long> ids = rdmMembers.stream().filter(rdmMember -> rdmMember.getGlAccessLevel() != 50).map(RdmMember::getId).collect(Collectors.toList());
+                            if (!contains) {
+                                //插入项目全局的权限
+                                RdmMember rdmMember = new RdmMember();
+                                rdmMember.setUserId(c7nUserVO.getId());
+                                rdmMember.setGlAccessLevel(RdmAccessLevel.OWNER.value);
+                                rdmMember.setSyncGitlabFlag(true);
+                                rdmMember.setType(AuthorityTypeEnum.GROUP.getValue());
+
+                                rdmMember.setOrganizationId(c7nTenantVO.getTenantId());
+                                C7nUserVO userVO = c7nBaseServiceFacade.detailC7nUserOnProjectLevel(projectId, c7nUserVO.getId());
+                                if (userVO != null) {
+                                    rdmMember.setGlUserId(userVO.getGitlabUserId().intValue());
+                                }
+                                rdmMember.setgGroupId(appGroupIdByProjectId.intValue());
+                                rdmMember.setProjectId(projectId);
+                                rdmMember.setSyncGitlabDate(new Date());
+                                rdmMemberMapper.insert(rdmMember);
+                            }
+                            if (!org.apache.commons.collections.CollectionUtils.isEmpty(ids)) {
+                                rdmMemberMapper.deleteByIds(ids);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+        LOGGER.info(">>>>>>>>>>>>>>>end fix member permission data>>>>>>>>>>>>>>>>>>");
+    }
 
 }
