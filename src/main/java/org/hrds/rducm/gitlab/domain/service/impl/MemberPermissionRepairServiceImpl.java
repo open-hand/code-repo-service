c0701f0c5f3092c0aa0537478fe333482d80a0e4
wangxiang <xiangwang04@hand-china.com>
2022-03-07 18:03:16 +0800

    [FIX] 修改事务的传播行为

--- src/main/java/org/hrds/rducm/gitlab/domain/service/impl/MemberPermissionRepairServiceImpl.java
+++ src/main/java/org/hrds/rducm/gitlab/domain/service/impl/MemberPermissionRepairServiceImpl.java
@@ -25,6 +25,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -49,7 +50,7 @@ public class MemberPermissionRepairServiceImpl implements IMemberPermissionRepai
     private Map<String, GitlabPermissionHandler> permissionRepairMap;
 
     @Override
-    @Transactional(rollbackFor = Exception.class)
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
     public void repairMemberPermission(Long organizationId) {
         List<RdmMemberAuditRecord> records = rdmMemberAuditRecordRepository
                 .selectByCondition(Condition.builder(RdmMemberAuditRecord.class)
